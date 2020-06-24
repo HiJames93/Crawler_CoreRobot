44b8f5384b0c719f009268892738c2e327b0afcc
HiJames93 <HiJames93@163.com>
2020-06-24 10:28:29 +0800

    first commit

---
+++ src/main/java/com/james/corerobot/CorerobotApplication.java
@@ -0,0 +1,13 @@
+package com.james.corerobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorerobotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorerobotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/james/corerobot/CorerobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.james.corerobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorerobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
